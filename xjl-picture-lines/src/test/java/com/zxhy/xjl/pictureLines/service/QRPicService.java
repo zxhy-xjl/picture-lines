0eb0bf40484728f8c7717a1be9587ebf6988393d
Fachion <lyyglin@163.com>
2016-06-28 14:59:07 +0800

    Create QRPicService.java

---
+++ xjl-picture-lines/src/test/java/com/zxhy/xjl/pictureLines/service/QRPicService.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.zxhy.xjl.picturelines.service;
+
+import java.io.File;
+
+/**
+ * @author LinHd
+ *
+ */
+public interface QRPicService {
+
+	/**
+	 * 生成附加指纹的二维码图片
+	 * @param text  生成QR码的文字
+	 * @param logoAddress  生成QR码上面的logo地址
+	 * @param newLogoAddress  将QR码和logo结合之后生成图片的地�?��可为�?
+	 * @param ifCompressed   是否压缩logo图片
+	 * @param picAddress     待生成二维码防伪图的图片地址
+	 * @param x     水印水平位置，以左上角为坐标原点
+	 * @param y     水印垂直位置，以左上角为坐标原点
+	 * @param alpha  水印透明�?0.1f-1.0f)
+	 * @param n   指纹重数
+	 */
+  public void getQRPic(String text,String logoAddress,String newLogoAddress,boolean ifCompressed,String picAddress,int x,int y,float alpha,int n);	
+	
+  /**
+   * 离线验证
+   * @param srcFile    原图片地�?
+   * @param destFile   目标图片地址
+   * @param n       指纹重数
+   */
+  public void offlineCheck(String srcFile, String destFile,int n);
+  
+  
+  /**
+   * 在线验证
+   */
+  public void onlineCheck();
+  
+}
